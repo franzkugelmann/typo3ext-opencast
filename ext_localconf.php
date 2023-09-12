38ec94fa525f8f10b902ff5bdb1ac850f962b527
Philipp Kitzberger <coding@kitze.net>
2023-09-12 09:29:28 +0200

    Applied rector rule: MethodGetInstanceToMakeInstanceCallRector

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,7 +6,7 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['onlineMediaHelpers']['opencast'] = \U
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['FileInfo']['fileExtensionToMimeType']['opencast'] = 'video/opencast';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['mediafile_ext'] .= ',opencast';
 
-$rendererRegistry = \TYPO3\CMS\Core\Resource\Rendering\RendererRegistry::getInstance();
+$rendererRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Resource\Rendering\RendererRegistry::class);
 $rendererRegistry->registerRendererClass(
     \Uos\Opencast\Rendering\OpencastRenderer::class
 );
