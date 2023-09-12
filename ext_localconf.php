ca3cffed71076a4a5987fff35be7211c6b7e11ad
Philipp Kitzberger <coding@kitze.net>
2023-09-12 09:30:17 +0200

    Applied rector rule: SubstituteConstantsModeAndRequestTypeRector
    https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/11.0/Deprecation-92947-DeprecateTYPO3_MODEAndTYPO3_REQUESTTYPEConstants.html

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3_MODE') || die('Access denied.');
+defined('TYPO3') || die('Access denied.');
 
 // Register opencast as media type
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['onlineMediaHelpers']['opencast'] = \Uos\Opencast\Helpers\OpencastHelper::class;
