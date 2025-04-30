189d16e47aa89d3eb441a9993c453a0f34b80470
Franz Kugelmann <franz.kugelmann@elementare-teilchen.de>
2025-04-30 08:53:27 +0200

    drop support for v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,6 +1,6 @@
 <?php
 
-$EM_CONF['opencast'] = [
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Opencast',
     'description' => 'Support for Opencast videos',
     'category' => 'plugin',
@@ -11,7 +11,7 @@ $EM_CONF['opencast'] = [
     'author_company' => 'Osnabrück University',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '12.4.0-12.4.99',
         ],
         'conflicts' => [
         ],
